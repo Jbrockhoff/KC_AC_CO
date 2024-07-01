13435196cf25b196cdc547ca40c5bcec48564373
ews578 <ewsnider28@yahoo.com>
2024-07-01 10:50:06 -0500

    updated services page buttons to redirect to each indivdual service page.

---
+++ Solved/src/components/pages/Heating.jsx
@@ -0,0 +1,13 @@
+// import { Link } from ' react-router-dom';
+// import Card from 'react-bootstrap/Card';
+
+function Heating() {
+    return (
+      <div>
+        <h1>Heating Services</h1>
+        {/* Add content specific to the Heating page */}
+      </div>
+    );
+  }
+  
+  export default Heating;
